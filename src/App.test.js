5d73d7b859b9d17168bc2e6ca16538d28b0653ec
pradeep chauhan <pradeep.chauhan_cs22@gla.ac.in>
2025-04-16 22:04:31 +0530

    Updatefiles

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TOP HEADLINES/i);
   expect(linkElement).toBeInTheDocument();
+  
+  const titleElement = screen.getByText(/No articles found/i);
+  expect(titleElement).toBeInTheDocument();
 });
